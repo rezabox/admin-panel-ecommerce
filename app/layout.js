1b1b36722d8d785ad4a72185fb9c0c883c92c1dc
rezabox <reza.asareh81@gmail.com>
2024-04-20 14:53:40 +0330

    updated app/layout

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,25 @@ import NextNprogress from "@/components/libraries/NextNprogress";
 import BootstrapClient from "@/components/libraries/Bootstrap";
 import Toastify from "@/components/libraries/Toastify";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import Navbar from "@/components/layout/Navbar";
+import Chart from "@/components/layout/Chart";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
         <NextNprogress>
-          {children}
+          <Header />
+          <div class="container-fluid">
+            <div class="row">
+              <Navbar/>
+              {children}
+              <Chart/>
+            </div>
+          </div>
           <BootstrapClient />
-          <Toastify/>
+          <Toastify />
         </NextNprogress>
       </body>
     </html>
