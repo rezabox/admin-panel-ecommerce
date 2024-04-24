f43ad1f92c8cf3988fe206aeeded8032c715bcf1
rezabox <reza.asareh81@gmail.com>
2024-04-24 08:51:12 +0330

    add action_form

---
+++ actions/auth.js
@@ -0,0 +1,78 @@
+"use server";
+
+import { postFetch, postFetchAuth } from "@/utils/fetch";
+import { handleError } from "@/utils/help";
+import { cookies } from "next/headers";
+
+async function login(stateLogin, formActionLogin) {
+  const email = formActionLogin.get("email");
+  const password = formActionLogin.get("password");
+  if (email === "" || password === "") {
+    return {
+      status: "error",
+      message: "ایمیل و پسورد را وارد کنید",
+    };
+  }
+  const data = await postFetchAuth("/auth/login", { email, password });
+  if (data.status === "success") {
+    cookies().set({
+      name: "token",
+      value: data.data.token,
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+    return {
+      status: data.status,
+      message: "ورود با موفقیت انجام شد.",
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handleError(data.message),
+    };
+  }
+}
+async function me(stateLogin, formActionLogin) {
+  const token = cookies().get("token");
+  if (!token) {
+    return {
+      error: "Not Authorized",
+    };
+  }
+  const data = await postFetch(
+    "/auth/me",
+    {},
+    { Authorization: `Bearer ${token.value}` }
+  );
+  if (data.status === "success") {
+    return {
+      user: data.data,
+    };
+  } else {
+    return {
+      error: "User Forbidden",
+    };
+  }
+}
+async function logout(stateLogout, formActionLogout) {
+  const token = cookies().get("token");
+  const data = await postFetch(
+    "/auth/logout",
+    {},
+    { Authorization: `Bearer ${token.value}` }
+  );
+  if (data.status === "success") {
+    cookies().delete("token");
+    return {
+      status: data.status,
+      message: "خروج با موفقیت انجام شد.",
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handleError(data.message),
+    };
+  }
+}
+export { login, me, logout };
