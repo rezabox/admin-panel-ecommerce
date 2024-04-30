70eac332101d4760bf41e1142a4067ca41de7646
rezabox <reza.asareh81@gmail.com>
2024-04-30 17:07:33 +0330

    updated style dashboard

---
+++ app/(dashboard)/Loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return(
+      <>
+        <div className="">
+              <div className="spinner-border d-flex"></div>
+        </div>
+      </>
+  )
+}
--- app/(dashboard)/users/page.js
+++ app/(dashboard)/users/page.js
@@ -1,20 +1,26 @@
-
-import PaginateTable from "@/components/users/paginateTable";
 import TableRes from "@/components/users/tableRes";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
+import Loading from "../Loading";
 
-function Userpage() {
+
+async function Userpage({ searchParams }) { 
+  const params = new URLSearchParams(searchParams);
   return (
-    <main class="col-md-9 ms-sm-auto col-lg-12 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h4 class="fw-bold">کاربران</h4>
-        <Link href='/users/create' type="button" class="btn btn-sm btn-outline-dark">
-               ایجاد کاربر
+    <main className="col-md-9 ms-sm-auto col-lg-12 px-md-4">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 className="fw-bold">کاربران</h4>
+        <Link
+          href="/users/create"
+          type="button"
+          className="btn btn-sm btn-outline-dark"
+        >
+          ایجاد کاربر
         </Link>
       </div>
-      <TableRes/>
-      <PaginateTable/>
+      <Suspense key={params.toString()} fallback={<Loading/>}>
+        <TableRes  params={params.toString()}/>
+      </Suspense>
     </main>
   );
 }
