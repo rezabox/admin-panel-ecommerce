c1e17898dd034c3a75c05aa828043091fef5cb07
rezabox <reza.asareh81@gmail.com>
2024-05-04 13:56:17 +0330

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+         protocol:'http',
+         hostname:'localhost',
+         port: '8000'
+      }
+    ]
+ }
 };
 
 export default nextConfig;
