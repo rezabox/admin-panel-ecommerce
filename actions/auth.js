733ce18a78ccbbebd1025907eea8e18f281a452c
rezabox <reza.asareh81@gmail.com>
2024-05-04 13:04:16 +0330

    create edit user formActions

--- actions/auth.js
+++ actions/auth.js
@@ -1,6 +1,12 @@
 "use server";
 
-import { deleteFetch, getFetch, postFetch, postFetchAuth } from "@/utils/fetch";
+import {
+  deleteFetch,
+  getFetch,
+  postFetch,
+  postFetchAuth,
+  putFetch,
+} from "@/utils/fetch";
 import { handleError } from "@/utils/help";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
@@ -78,16 +84,16 @@ async function logout(stateLogout, formActionLogout) {
     };
   }
 }
-async function createUser(stateUser,formCreateUser) {
-  const name = formCreateUser.get('name');
-  const email = formCreateUser.get('email');
-  const cellphone = formCreateUser.get('cellphone');
-  const password = formCreateUser.get('password');
-  if(name === '' || email === '' || cellphone === '' || password === ''){
-     return {
-        status: 'error',
-        message: "تمامی فیلد های زیر را پر کنید."
-     }
+async function createUser(stateUser, formCreateUser) {
+  const name = formCreateUser.get("name");
+  const email = formCreateUser.get("email");
+  const cellphone = formCreateUser.get("cellphone");
+  const password = formCreateUser.get("password");
+  if (name === "" || email === "" || cellphone === "" || password === "") {
+    return {
+      status: "error",
+      message: "تمامی فیلد های زیر را پر کنید.",
+    };
   }
   const pattern = /^(\+98|0)?9\d{9}$/;
   if (!pattern.test(cellphone)) {
@@ -96,10 +102,10 @@ async function createUser(stateUser,formCreateUser) {
       message: "فرمت شماره موبایل درست نیست.",
     };
   }
-  const data = await postFetch('/users', {name, email, cellphone , password})
-  
+  const data = await postFetch("/users", { name, email, cellphone, password });
+
   if (data.status === "success") {
-    revalidatePath('/users')
+    revalidatePath("/users");
     return {
       status: data.status,
       message: "کاربر جدید اضافه شد.",
@@ -111,29 +117,80 @@ async function createUser(stateUser,formCreateUser) {
     };
   }
 }
-async function deleteForm(stateDeleteUser,formActionDelete){
-   const id = formActionDelete.get('userId');
-   if(id === '' || id === null){
-      return {
-          status: "error",
-          message: "شناسه کاربر الزامی است."
-      }
-   }   
-   const data = await deleteFetch(`/users/${id}`)
-   if (data.status === "success") {
-     revalidatePath("/users");
-     redirect("/users");
-     return {
-       status: data.status,
-       message: "حذف کاربر با موفقیت انجام شد.",
-      }
+async function deleteForm(stateDeleteUser, formActionDelete) {
+  const id = formActionDelete.get("userId");
+  if (id === "" || id === null) {
+    return {
+      status: "error",
+      message: "شناسه کاربر الزامی است.",
+    };
+  }
+  const data = await deleteFetch(`/users/${id}`);
+  if (data.status === "success") {
+    revalidatePath("/users");
+    redirect("/users");
+    return {
+      status: data.status,
+      message: "حذف کاربر با موفقیت انجام شد.",
+    };
   } else {
     return {
       status: data.status,
       message: handleError(data.message),
     };
   }
+}
+async function updatedForm(stateUpdateUser, formActionUpdate) {
+  const name = formActionUpdate.get("name");
+  const email = formActionUpdate.get("email");
+  const cellphone = formActionUpdate.get("cellphone");
+  const password = formActionUpdate.get("password");
+  const id = formActionUpdate.get("id");
 
+  if (id === "" || id === null) {
+    return {
+      status: "error",
+      message: "شناسه کاربر الزامی است.",
+    };
+  }
+  if (name === "" || name === null) {
+    return {
+      status: "error",
+      message: "فیلد نام را پر کنید",
+    };
+  }
+  if (email === '' || email === null) {
+    return {
+      status: "error",
+      message: "فیلد ایمیل را پر کنید",
+    };
+  }
+  const pattern = /^(\+98|0)?9\d{9}$/;
+  if (!pattern.test(cellphone)) {
+    return {
+      status: "error",
+      message: "فرمت شماره موبایل درست نیست.",
+    };
+  }
+
+  const data = await putFetch(`/users/${id}`, {
+    name,
+    email,
+    cellphone,
+    password,
+  });
+  if (data.status === "success") {
+    revalidatePath("/users");
+    return {
+      status: data.status,
+      message: "ویرایش کاربر با موفقیت انجام شد.",
+    };
+  } else {
+    return {
+      status: data.status,
+      message: handleError(data.message),
+    };
+  }
 }
 
-export { login, me, logout , createUser, deleteForm};
+export { login, me, logout, createUser, deleteForm, updatedForm };
